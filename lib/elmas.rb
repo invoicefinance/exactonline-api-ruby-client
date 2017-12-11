ed5038f8d514258db4f284538a283d06dd7da004
Marco Braak <marco@invoicefinance.nl>
2017-12-11 14:46:40 +0100

    Add Elmas::Bulk::TransactionLine

--- lib/elmas.rb
+++ lib/elmas.rb
@@ -54,6 +54,7 @@ require "elmas/resources/general_journal_entry"
 require "elmas/resources/general_journal_entry_line"
 require "elmas/resources/payment_condition"
 require "elmas/resources/division"
+require "elmas/resources/bulk/transaction_line"
 
 module Elmas
   extend Config
